cd024671811339d9165acd2d2dbb0aa5933b7f96
Johnatan vieira <107522911+Hiiits@users.noreply.github.com>
2025-08-24 14:54:25 -0300

    change url ws

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -88,7 +88,7 @@ const handleLogin = (event) => {
 login.style.display = "none"
 chat.style.display = "flex"
 
-websocket = new WebSocket("https://chat-dos-baile-backend.onrender.com")
+websocket = new WebSocket("ws://chat-dos-baile-backend.onrender.com")
 websocket.onmessage = processMessage
 
 
